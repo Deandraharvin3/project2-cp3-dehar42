f9c0c24ef07d0fdbe34c09b3664d6619473305d9
Ubuntu <ubuntu@ip-172-31-33-191.ec2.internal>
2019-09-14 15:35:53 +0000

    Forgot to commit after making my chatbox layout, but I will be changing it, now I have installed socket.io and want to commit

--- scripts/Content.js
+++ scripts/Content.js
@@ -1,8 +1,38 @@
     
 import * as React from 'react';
 
+var http = require("https");
+
+var options = {
+	"method": "GET",
+	"hostname": "api.chatbot.com",
+	"port": null,
+	"path": "/stories",
+	"headers": {
+		"authorization": "Bearer ${MiYLzDMtDWPZB2cBdatNme6Jdbr86N0_}"
+	}
+};
+
+var req = http.request(options, function (res) {
+	var chunks = [];
+
+	res.on("data", function (chunk) {
+		chunks.push(chunk);
+	});
+
+	res.on("end", function () {
+		var body = Buffer.concat(chunks);
+		console.log(body.toString());
+	});
+});
+
+req.end();
+
 export class Content extends React.Component {
     render() {
-        return <div><h1>Hello World from React!</h1></div>;
+        return <div><h1>Welcome to the Chat Bot!</h1>
+        <body></body>
+        <input />
+        <button>Send</button></div>;
     }
 }
